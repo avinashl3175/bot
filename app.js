527c9056172987c23960a89057c967ed18bab08e
Avinash Kumar <avinashk@vmokshagroup.com>
2017-01-02 12:39:38 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -10,16 +10,21 @@ app.use(bodyParser.json());
 
 // define the home page route, running index.html when server runs
 app.post('/api/luis', function (req, res, next) {
-    var message = req.body.Message;
-    var luisEndPoint = "https://api.projectoxford.ai/luis/v2.0/apps/9e8003f2-e927-4051-b18c-d4ad1ad0a807?subscription-key=c45dc99ded6c4789b9d5264218fc4c5a&verbose=true&q=" + message;
-    request(luisEndPoint, function (error, response, body) {
-        if (error) {
-            res.send("Error" + error);;
-        }
-        else {
-            res.send(body);
-        }
-    });
+    try{
+        var message = req.body.Message;
+        var luisEndPoint = "https://api.projectoxford.ai/luis/v2.0/apps/9e8003f2-e927-4051-b18c-d4ad1ad0a807?subscription-key=c45dc99ded6c4789b9d5264218fc4c5a&verbose=true&q=" + message;
+        request(luisEndPoint, function (error, response, body) {
+            if (error) {
+                res.send("Error" + error);
+            }
+            else {
+                res.send(body);
+            }
+        });
+    }
+    catch (e) {
+        res.send("Error" + e);
+    }   
 });
 
 //Setting up server
